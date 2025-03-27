fbba92c845e11e26c80a6a3d0ec5b3b09ce7dbc7
dariodaddamio <dario.daddamio@gmail.com>
2025-03-26 18:47:44 -0700

    fix: import statements

--- Frontend/src/pages/membership/ProcessFlow.tsx
+++ Frontend/src/pages/membership/ProcessFlow.tsx
@@ -1,5 +1,5 @@
-import { ProcessStep } from "./ProcessStep";
-import { ProcessArrow } from "./ProcessArrow";
+import { ProcessStep } from "../../components/ui/ProcessStep";
+import { ProcessArrow } from "../../components/ui/ProcessArrow";
 import Navbar from "../../components/layout/Navbar";
 import Footer from "../../components/layout/Footer";
 export const ProcessFlow = () => {
