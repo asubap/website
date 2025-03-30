4601a5ce1216cb8cc1aed0cca98b8693f14f6bf2
asubap <tech.asubap@gmail.com>
2025-03-30 14:26:59 -0700

    Update App.tsx

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -10,7 +10,7 @@ import { Navigate, Outlet } from "react-router-dom";
 function App() {
   // Protected route example
   const ProtectedRoute = () => {
-    const {loading} = useAuth();
+    const {user, loading} = useAuth();
     if (loading) return <p>Loading...</p>;
     return user ? <Outlet/> : <Navigate to="/login" replace/>;
   }
