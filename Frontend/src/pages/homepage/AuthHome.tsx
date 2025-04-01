b53a2123a1031c12eaa6915fed3a225fc662acae
dittygoops <aditya.gup16@gmail.com>
2025-04-01 16:34:11 -0700

    some admin page

--- Frontend/src/pages/homepage/AuthHome.tsx
+++ Frontend/src/pages/homepage/AuthHome.tsx
@@ -2,6 +2,7 @@ import { supabase } from "../../context/auth/supabaseClient";
 import { useAuth } from "../../context/auth/authProvider";
 
 import Admin from "../admin/Admin";
+import SponsorHome from "../sponsor/SponsorHome";
 
 export default function AuthHome() {    
     const { session } = useAuth();
@@ -9,18 +10,21 @@ export default function AuthHome() {
     const { role } = useAuth(); // Call hooks at the top level
 
     const handleSignOut = async () => {
+      console.log(role);
       await supabase.auth.signOut();
       window.location.href = "/";
     };
 
     return (
       <>
-        {role == "e-board" ? (
-          <Admin></Admin>
+        {role === "e-board" ? (
+          <Admin />
+        ) : role === "sponsor" ? (
+          <SponsorHome />
         ) : (
           <div>
             <h1 className="text-3xl font-bold text-center">Welcome, {user?.email}!</h1>
-            <p className="text-center">You are logged in as a member. {role ? role.toString() : ''}</p>
+            <p className="text-center">You are logged in as a member. Role: {role ? role.toString() : ''}</p>
             <button
               className="mt-4 px-4 py-2 bg-red-500 text-white rounded"
               onClick={handleSignOut}
