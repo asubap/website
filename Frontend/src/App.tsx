bd412b22a9a00062a8c8b0a728a3b67cc7a63c3b
dariodaddamio <dario.daddamio@gmail.com>
2025-03-31 17:28:55 -0700

    fix: updated routing

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -2,11 +2,13 @@ import "./App.css";
 import { ProcessFlow } from "./pages/membership/ProcessFlow";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Homepage from "./pages/homepage/HomePage";
-import LogInPage from "./pages/login/LoginPage";
+import LogInPage from "./pages/login/LogInPage";
 import { AuthProvider } from "./context/auth/authProvider";
 import AuthHome from "./pages/homepage/AuthHome";
 import { useAuth } from "./context/auth/authProvider";
 import { Navigate, Outlet } from "react-router-dom";
+import AboutPage from "./pages/about/AboutPage";
+import SponsorsPage from "./pages/sponsors/SponsorsPage";
 function App() {
   // Protected route example
   const ProtectedRoute = () => {
@@ -21,6 +23,8 @@ function App() {
           <Routes>
             <Route path="/login" element={<LogInPage />} />
             <Route path="/membership" element={<ProcessFlow />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/sponsors" element={<SponsorsPage />} />
             <Route path="/" element={<Homepage />} />
             <Route element={<ProtectedRoute />}>
               <Route path="/auth/Home" element={<AuthHome />} />
