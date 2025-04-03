3baf482f1b503b12faa77bd6a25d52bf139b65c1
dittygoops <aditya.gup16@gmail.com>
2025-04-03 16:41:03 -0700

    fixes

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -3,7 +3,6 @@ import { ProcessFlow } from "./pages/membership/ProcessFlow";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Homepage from "./pages/homepage/HomePage";
 import LogInPage from "./pages/login/LogInPage";
-import { AuthProvider } from "./context/auth/authProvider";
 import AuthHome from "./pages/homepage/AuthHome";
 import { Navigate, Outlet } from "react-router-dom";
 
