6e3eff1ef6252041625e775ac480df5e32224ab9
abhave1 <abhaveabhilash@gmail.com>
2025-04-08 00:12:32 -0700

    removing uncessary files

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -3,17 +3,14 @@ import cors from "cors";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import routes from "./routes";
-import path from "path";
 
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
 
 // Mount all routes
 app.use("/", routes);
