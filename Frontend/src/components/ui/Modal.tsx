06c7f5deade2a9c951ae0485f07e018354b3771d
asubap <tech.asubap@gmail.com>
2025-04-13 17:06:11 -0700

    Update Modal.tsx

--- Frontend/src/components/ui/Modal.tsx
+++ Frontend/src/components/ui/Modal.tsx
@@ -33,7 +33,7 @@ const Modal: React.FC<ModalProps> = ({
 }) => {
   const modalRef = useRef<HTMLDivElement>(null);
   const [showUnsavedChangesConfirmation, setShowUnsavedChangesConfirmation] = useState(false);
-  const [closeTrigger, setCloseTrigger] = useState<'x' | 'outside' | 'button' | 'keyboard' | null>(null);
+  const [setCloseTrigger] = useState<'x' | 'outside' | 'button' | 'keyboard' | null>(null);
 
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
