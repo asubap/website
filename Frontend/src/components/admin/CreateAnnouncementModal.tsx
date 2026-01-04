ec341dc934730111ed3b6766d52d4cd78d5e4b63
ashcodex505 <ashishkurse@gmail.com>
2026-01-03 22:26:36 -0700

    took off the trial premium features so that its just the essential core features that show

--- Frontend/src/components/admin/CreateAnnouncementModal.tsx
+++ Frontend/src/components/admin/CreateAnnouncementModal.tsx
@@ -238,10 +238,7 @@ const CreateAnnouncementModal = ({
         plugins: [
           // Core editing features
           'anchor', 'autolink', 'charmap', 'codesample', 'emoticons', 'link', 'lists', 'media', 'searchreplace', 'table', 'visualblocks', 'wordcount',
-          // Your account includes a free trial of TinyMCE premium features
-          // Try the most popular premium features until Jan 15, 2026:
-          'checklist', 'mediaembed', 'casechange', 'formatpainter', 'pageembed', 'a11ychecker', 'tinymcespellchecker', 'permanentpen', 'powerpaste', 'advtable', 'advcode', 'advtemplate', 'ai', 'uploadcare', 'mentions', 'tinycomments', 'tableofcontents', 'footnotes', 'mergetags', 'autocorrect', 'typography', 'inlinecss', 'markdown','importword', 'exportword', 'exportpdf'
-        ],
+           ],
         toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography uploadcare | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
         tinycomments_mode: 'embedded',
         tinycomments_author: 'Author name',
